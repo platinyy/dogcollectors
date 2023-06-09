65e1f1948eb8ef4bc1f1d99bb0be385ac8aa79f6
Platin Syla <platinsyla06@gmail.com>
2023-06-09 22:20:10 +0200

    Dogs collector part 1 finished

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+dogs = [
+  {'name': 'Duke', 'breed': 'bulldog', 'description': 'well-muscled bruiser', 'age': 3},
+  {'name': 'Bailey', 'breed': 'Shiba Inu', 'description': 'favorite coin', 'age': 2},
+  {'name': 'Luna', 'breed': 'Bichon Frise', 'description': 'Sweetiest puppy', 'age': 0},
+]
+
 # Create your views here.
 
 def home(request):
@@ -7,3 +13,9 @@ def home(request):
   return render(request, 'home.html')
 def about(request):
   return render(request, 'about.html')
+
+def dogs_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'dogs/index.html', {
+    'dogs': dogs
+  })
