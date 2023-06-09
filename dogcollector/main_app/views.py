387a88da68eb580565d763425d13d01246466bd2
Platin Syla <platinsyla06@gmail.com>
2023-06-09 21:46:04 +0200

    Added css styling

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+def about(request):
+  return render(request, 'about.html')
