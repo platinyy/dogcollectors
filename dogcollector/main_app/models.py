41b1fabb26a499ce3c93f276ea22478a7b0781b0
Platin Syla <platinsyla06@gmail.com>
2023-06-15 12:15:14 +0200

    Dog collector lab part 4 finished

--- dogcollector/main_app/models.py
+++ dogcollector/main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -12,4 +18,21 @@ class Dog(models.Model):
        return f'{self.name}'
     
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'dog_id': self.id})
+       return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+	      choices=MEALS,
+	      default=MEALS[0][0]
+  )
+    
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
